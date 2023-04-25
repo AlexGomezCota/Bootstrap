b3a10a99f2eead0830d5916f1c66a34e8eed1848
Alex Gomez <alejandrogomezcota@gmail.com>
2023-04-25 15:52:18 -0600

    ya con la linkeada

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ function changeColorToRed(){
 
 refName.style.color = "red";
 }
+function changeColorToBlue(){
+    const refName= document.getElementById("name");
+
+refName.style.color = "blue";
+}
+function changeColorToGreen(){
+    const refName= document.getElementById("name");
+
+refName.style.color = "green";
+}
+function changeColorP(color, refObj){
+    console.log(refObj);
+    refObj.style.color = color;
+}
+function resetColors(){
+    const refObjs = document.getElementsByClassName("text-color");
+    console.log(refObjs);
+    for(let index=0; index < refObjs.length; index++){
+        const element= refObjs[index];
+        element.style.color = "black";
+    } 
+}
